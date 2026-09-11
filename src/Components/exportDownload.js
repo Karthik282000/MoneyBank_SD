5d71e59e089f509627e48070a329dc7d64f7e0bb
Karthik282000 <skarthikvds@gmail.com>
2026-09-11 14:49:26 +0530

    Excel xheck deployment

--- src/Components/exportDownload.js
+++ src/Components/exportDownload.js
@@ -96,8 +96,13 @@ function zipStore(files) {
 }
 
 function xmlEscape(value) {
-  return String(value ?? '')
-    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F]/g, '')
+  const cleaned = Array.from(String(value ?? ''))
+    .filter((ch) => {
+      const code = ch.charCodeAt(0);
+      return code === 9 || code === 10 || code === 13 || code >= 32;
+    })
+    .join('');
+  return cleaned
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
